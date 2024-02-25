e010362b38f2dfe020f29cae01316bd5a7e27825
Aiden Wu <000479685@student.vcc.ca>
2024-02-24 16:33:39 -0800

    project more or less done

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 let data = [];
 const moviesContainer = document.getElementById("movie-container");
+let genreData = [];
+
 
 async function getMovies()
 {
     const moviesData = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=20e0bee41c4c344e8776905f04ac87b6");
     const moviesDataJSON = await moviesData.json();
     data = moviesDataJSON.results;
-    generatePopularMoviesUI();
+
+    const genreFetch = await fetch("http://api.themoviedb.org/3/genre/movie/list?api_key=20e0bee41c4c344e8776905f04ac87b6");
+    const genreFetchJSON = await genreFetch.json();
+    genreData = genreFetchJSON.genres;
+
+    generateMoviesUI();
 }
 
 
 
-function createMovie(movie)
+async function getPopularMovies()
 {
+    const moviesData = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=20e0bee41c4c344e8776905f04ac87b6");
+    const moviesDataJSON = await moviesData.json();
+    data = moviesDataJSON.results;
+    generateMoviesUI();
+}
+
+async function getTopRatedMovies()
+{
+    const moviesData = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=20e0bee41c4c344e8776905f04ac87b6");
+    const moviesDataJSON = await moviesData.json();
+    data = moviesDataJSON.results;
+    generateMoviesUI();
+}
+
+async function getUpcomingMovies()
+{
+    const moviesData = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=20e0bee41c4c344e8776905f04ac87b6");
+    const moviesDataJSON = await moviesData.json();
+    data = moviesDataJSON.results;
+    generateMoviesUI();
+}
+
+function createMovie(movie)
+{    
     const bannerUI = `
-    <div class="basis-1/6 rounded shadow-lg">
-    <img class="w-full" src=${"https://image.tmbd.org/t/p/w500" + movie.poster_path} alt="The movie poster here">
+    <div class="flex-1 basis-1/4">
+    <img class="w-1/4 min-h-40 max-h-96" src=${"https://image.tmbd.org/t/p/w500" + movie.poster_path} alt="The movie poster here">
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">${movie.original_title}</div>
       <p class="text-gray-700 text-base">
@@ -28,9 +59,7 @@ function createMovie(movie)
       }</span>
       <div>Genre overview: ${createGenreListings(movie.genre_ids)}
 </div>
-       <div class="mt-4">
-       ${movie?.borders?.length ? createBorderChips(movie.borders) : ""}
-       </div>
+
     </div>
   </div>
     `;
@@ -38,28 +67,20 @@ function createMovie(movie)
     moviesContainer.innerHTML += bannerUI;
 }
 
-function createBorderChips(borders) 
-{
-    const items = borders.map((border) => {
-    return `<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">${border}</span>`;
-});
-
-    return items.join("");
-}
-
 function createGenreListings(genreList)
 {
     let liHtml = ``;
     genreList.forEach(element => {
-        liHtml += `<li>${element}`
+        liHtml += `<li>${genreData[genreData.map(e => e.id).indexOf(element)].name}`
     });
     return liHtml;
 }
 
 
 
-function generatePopularMoviesUI()
+function generateMoviesUI()
 {
+    moviesContainer.innerHTML = "";
     for(let movie of data)
     {
         createMovie(movie);
